b5ea7fa570ddbbefc9d77f5e773a0a8944d538d4
Bryan Ndum <bryanstructland@gmail.com>
2026-05-06 16:22:33 +0100

    Hello Program

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello Everyone, I am learning java");
+		System.out.print("Nah %n");
+		System.out.printf("Do you love Java %c %b",'?',true);
+	}
+	
+}
