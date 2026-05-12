55b5354677dfa78fa2747d1953df5a7726049242
Bryan Ndum <bryanstructland@gmail.com>
2026-05-12 15:11:56 +0100

    Java Assignment

---
+++ UserInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UserInput{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your name: ");
+		String name = input.nextLine();
+		
+		System.out.print("Enter your gender: ");
+		String gender = input.next();
+		
+		System.out.print("Enter your age: ");
+		byte age = input.nextByte();
+		
+		System.out.print("Enter the amount of students in your class: ");
+		short classStudents = input.nextShort();
+		
+		System.out.printf("Hello %s. You are welcome to NIIT %n",name);
+		System.out.printf("You are a %s and you are %d years old %n",gender,age);
+		System.out.printf("There are %d students in your class",classStudents);
+	}
+}
